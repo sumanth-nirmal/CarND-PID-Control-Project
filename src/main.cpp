2aa533fda51cc2ce1e3a920da4b308f39f702f63
sumanth <sumanth.724@gmail.com>
2017-07-12 17:36:35 -0700

    added throttle

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ int main()
   uWS::Hub h;
 
   PID pid;
-  pid.Init(0.085, 0, 2);
+  pid.Init(0.15, 0.0001, 1);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -51,28 +51,30 @@ int main()
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value;
+          double throttle_value;
           /*
           * TODO: Calcuate steering value here, remember the steering value is
           * [-1, 1].
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+            // update the error value to the controller
           pid.UpdateError(cte);
           steer_value = -pid.TotalError();
 
-          // limit steer value within -1 and 1
+          // bound the steering values
           if (steer_value > 1.0) steer_value = 1.0;
           if (steer_value < -1.0) steer_value = -1.0;
 
-          // when steer value is within a threshold, use larger throttle
-          //throttle_value = (fabs(steer_value) < steer_thre)? throttle_high:throttle_low;
+          // full throttle when driving almost striaght
+          throttle_value = (fabs(steer_value) < 0.03)? 0.7 : 0.25;
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = throttle_value;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
