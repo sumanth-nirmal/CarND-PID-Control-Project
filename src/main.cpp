10c7c02f6eaff4229e7a8dbd90ecf8dc10ea9899
sumanth <sumanth.724@gmail.com>
2017-07-17 11:57:35 -0700

    final parameters

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ int main()
   uWS::Hub h;
 
   PID pid;
-  pid.Init(0.1, 0.0001, 3.2);
+  pid.Init(0.15, 0.0002, 3.8);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
