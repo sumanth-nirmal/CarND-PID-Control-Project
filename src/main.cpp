2d16f031f3dddf7c201840d1fab6e3a7056a8c04
sumanth <sumanth.724@gmail.com>
2017-07-17 11:45:13 -0700

    updated the parameters

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ int main()
   uWS::Hub h;
 
   PID pid;
-  pid.Init(0.15, 0.0001, 1);
+  pid.Init(0.1, 0.0001, 3.2);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -58,6 +58,7 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+
             // update the error value to the controller
           pid.UpdateError(cte);
           steer_value = -pid.TotalError();
